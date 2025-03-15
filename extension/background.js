cb1bf3dea07fcff1b37bfb859fe7739cb9e080c3
Amir Zak <amirzak6@gmail.com>
2025-03-15 23:09:20 +0200

    Added icons and decide on final name

--- extension/background.js
+++
@@ -1,6 +0,0 @@
-// Listen for messages from content script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'openPopup') {
-        chrome.action.openPopup();
-    }
-}); 
